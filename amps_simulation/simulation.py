1c942826e0cbb407f6faa6b1cc5348a08c3986d8
Davido <d.pogorzelski91@gmail.com>
2025-04-18 11:26:44 +0200

    add json import

--- amps_simulation/simulation.py
+++ amps_simulation/simulation.py
@@ -3,6 +3,7 @@ import numpy as np
 from amps_simulation.core.state_space_model import extract_differential_equations, simulate_circuit
 from amps_simulation.core.utils import plot_results
 import logging
+import json
 
 
 logging.basicConfig(
@@ -27,8 +28,9 @@ def run_simulation_from_file(file_path):
 
 def run_simulation(circuit_json_data):
 
+    print(circuit_json_data)
     # Extract state space matrices
-    A, B, state_vars = extract_differential_equations(circuit_json_data)
+    A, B, state_vars, input_vars = extract_differential_equations(circuit_json_data)
 
     # Define identity output matrix C (observing all state variables)
     C = np.eye(A.shape[0])  # Identity matrix of size (states x states)
@@ -38,14 +40,19 @@ def run_simulation(circuit_json_data):
     t_span = (0, 10)  # Simulate from 0 to 10 seconds
     initial_conditions = np.zeros(A.shape[0])  # Zero initial state
 
+
+    ### TODO actual inputs
     # Define step input function (5V at t >= 1s)
-    def step_input_function(t):
-        return 5.0 if t >= 1 else 0.0  # Single input, no list needed
+    def step_input_function(t, n = len(input_vars.keys())):
+        return np.array([5.0 if t >= (i + 1) else 0.0 for i in range(n)])
+
 
     # Solve the ODE system
     t, x, y = simulate_circuit(A, B, C, t_span, initial_conditions, step_input_function)
+    logging.info("✅ Time points: %s", t[0:10])  # Log first 10 time points
+    logging.info("✅ Simulation completed.")
     # Plot the results
-    #plot_results(t, x, state_vars)
+    plot_results(t, x, state_vars)
 
 
 
