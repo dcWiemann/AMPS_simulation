8f2b6fcc6ccfac846bcc6ec4d0da5045ab1a2603
Dorothea C. Wiemann <d.wiemann@tu-berlin.de>
2025-04-20 18:26:59 +0200

    change test file to rlc circuit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from amps_simulation.run_simulation import run_simulation_from_file
 
 if __name__ == "__main__":
-    run_simulation_from_file('test_data/test_lcr.json') 
+    run_simulation_from_file('test_data/test_rlc.json') 
