d61774f8f64ccdcf226a250646a70db387dcdecc
Dorothea C. Wiemann <d.wiemann@tu-berlin.de>
2025-04-22 10:23:55 +0200

    adapt __init__ files to new class structure

--- amps_simulation/__init__.py
+++ amps_simulation/__init__.py
@@ -1,10 +1,13 @@
-from .core.equations import *
-from .core.parsing import *
-from .core.state_space_model import *
-from .core.utils import *
+from .core import ElectricalModel, CircuitParser, ParserJson, Simulation
 from .run_simulation import run_simulation
 
 __version__ = "0.1.0"
 
-# Export the main functions that users will need
-__all__ = ['run_simulation']
+# Export the main classes and functions that users will need
+__all__ = [
+    'ElectricalModel',
+    'CircuitParser',
+    'ParserJson',
+    'Simulation',
+    'run_simulation'
+]
--- amps_simulation/core/__init__.py
+++ amps_simulation/core/__init__.py
@@ -1,52 +1,22 @@
 # amps_simulation/core/__init__.py
 
-# Import from equations.py
-from .equations import (
-    extract_input_and_state_vars,
-    write_kcl_equations,
-    write_kvl_equations,
-    find_loops,
-    solve_helper_variables,
-    solve_state_derivatives,
-    extract_state_space_matrices,
-    substitute_component_values
-)
+# Import from electrical_model.py
+from .electrical_model import ElectricalModel
 
-# Import from parsing.py
-from .parsing import (
-    build_electrical_nodes,
-    build_circuit_components,
-    assign_voltage_variables,
-    assign_current_variables
-)
+# Import from parser.py
+from .parser import CircuitParser, ParserJson
 
-# Import from state_space_model.py
-from .state_space_model import (
-    extract_differential_equations,
-    simulate_circuit
-)
+# Import from simulation.py
+from .simulation import Simulation
 
 # Import from utils.py
+from .utils import *
 
 # Define what should be available when someone imports from amps_simulation.core
 __all__ = [
-    # From equations.py
-    'extract_input_and_state_vars',
-    'write_kcl_equations',
-    'write_kvl_equations',
-    'find_loops',
-    'solve_helper_variables',
-    'solve_state_derivatives',
-    'extract_state_space_matrices',
-    'substitute_component_values',
-    
-    # From parsing.py
-    'build_electrical_nodes',
-    'build_circuit_components',
-    'assign_voltage_variables',
-    'assign_current_variables',
-    
-    # From state_space_model.py
-    'extract_differential_equations',
-    'simulate_circuit',
+    # Main classes
+    'ElectricalModel',
+    'CircuitParser',
+    'ParserJson',
+    'Simulation',
 ]
